622b24f1644ef6fd8c1602fcbb4c620b50f96959
Surya <ajithn@berarkrays.com>
2023-08-16 18:06:08 +0530

    Email Configration

--- src/components/Talk.js
+++ src/components/Talk.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import image from "../assets/Strategic consulting-amico.png";
 import Image from "next/image";
+import EmailJSResponseStatus from "@emailjs/browser";
 
 export default function Talk() {
+  const [sendEmails, setSendEmails] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [emailSendSucess, setEmailSendSucess] = useState(false);
+
+  // useEffect(() => {
+  //   alert(JSON.stringify(sendEmails));
+  // }, [sendEmails]);
+
+  const sendEmail = () => {
+    alert(JSON.stringify(sendEmails));
+    EmailJSResponseStatus.send(
+      "suryaKS",
+      "template_nu5zrvh",
+      sendEmails,
+      "Had0ERUPI9600d0lv"
+    ).then(
+      function (response) {
+        console.log("SUCCESS!", response);
+        setSendEmails({ name: "", email: "", message: "" });
+        setEmailSendSucess(true);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+  };
   return (
     <div className=" md:px-20 px-10 text-center my-20 ">
       <h1 className=" text-3xl font-bold my-20">Let's Talk</h1>
@@ -11,27 +41,55 @@ export default function Talk() {
           {/* <form action="" className="block w-full"> */}
           <input
             type="text"
-            className="h-10 rounded-xl px-2"
+            className="h-10 rounded-xl px-2 text-black"
             placeholder="Name"
+            onChange={(e) => {
+              setSendEmails({
+                ...sendEmails,
+                name: e.target.value,
+              });
+            }}
+            value={sendEmails.name}
           />
           <input
             type="email"
-            className="h-10 rounded-xl px-2"
+            className="h-10 rounded-xl px-2 text-black"
             placeholder="Email"
+            onChange={(e) => {
+              setSendEmails({ ...sendEmails, email: e.target.value });
+            }}
+            value={sendEmails.email}
           />
           <textarea
             name=""
             id=""
             cols="30"
             rows="10"
-            className="h-20 rounded-xl px-2 p-2"
+            className="h-20 rounded-xl px-2 p-2 text-black"
             placeholder="Message..."
+            onChange={(e) => {
+              setSendEmails({
+                ...sendEmails,
+                message: e.target.value,
+              });
+            }}
+            value={sendEmails.message}
           ></textarea>
+          {emailSendSucess === true && (
+            <p className="text-green-400 font-extrabold">
+              {" "}
+              Mail has been send...
+            </p>
+          )}
           <button className="  flex justify-center">
-            <span className="border-none rounded-xl bg-[#1BA40C] p-2 px-5 mt-5">
+            <span
+              className="border-none rounded-xl bg-[#1BA40C] p-2 px-5 mt-5"
+              onClick={sendEmail}
+            >
               Submit
             </span>
           </button>
+
           {/* </form> */}
         </div>
         <div className="md:flex hidden">
